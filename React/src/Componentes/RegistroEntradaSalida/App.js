35fdc4d45fdc0bff4de01a4aa7755d5c4365a009
Josebaigarcia <josebaigarcia@gmail.com>
2021-12-02 17:53:46 +0100

    Proyecto React

---
+++ React/src/Componentes/RegistroEntradaSalida/App.js
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import './App.css';
+
+function RegistroEntradaSalidaComponent() {
+  const [dni, setdni] = useState("1897171da")
+
+  function cambiarDni(event) {
+    setdni(event.target.value)
+  }
+
+  return (
+    <div className="App">
+      <label>DNI</label>
+      <input type="text" value={dni} onChange={cambiarDni} />
+      <button onClick={() => registrarEntrada(dni)} >Entrada</button>
+      <button onClick={() => registrarSalida(dni)} >Salida</button>
+    </div>
+  );
+}
+
+export default RegistroEntradaSalidaComponent;
+
+async function registrarEntrada(dni) {
+  let id = await obtenerIdUsuario(dni)
+  if (id !== undefined) {
+    await updateEntrada(id, true);
+  }
+
+}
+
+async function registrarSalida(dni) {
+  let id = await obtenerIdUsuario(dni);
+  if (id !== undefined) {
+    await updateEntrada(id, false);
+  }
+}
+
+async function obtenerIdUsuario(dni) {
+  let response = await fetch(`http://localhost:55434/listaUsuarios?DNI=${dni}`);
+  let respuesta = await response.json();
+  console.log(respuesta[0]._id)
+  return respuesta[0]._id
+}
+
+async function updateEntrada(idUsuario, esEntrada) {
+  let response = await fetch(`http://localhost:55434/actualizarUsuario`, {
+    method: 'PUT',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ Id: idUsuario, estaEnOficina: esEntrada })
+  })
+  let respuesta = await response.json();
+  console.log(respuesta)
+  return respuesta;
+}
--- React/src/index.js
+++ React/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import RegistroEntradaSalidaComponent from './Componentes/RegistroEntradaSalida/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+    <RegistroEntradaSalidaComponent />
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
